1311d0512cdaab3f4a1cdac6480b1f45261c1251
Kevin Prichard <kprichard@gmail.com>
2024-04-09 17:45:10 -0700

    initial add of non-working rtsp startup script

---
+++ start_rtsp.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+rpicam-vid \
+--autofocus-mode continuous \
+--inline 1 \
+--brightness 0.1 \
+--contrast 1.0 \
+--denoise cdn_off \
+--sharpness 1.0 \
+--level 4.1 \
+--framerate 25 \
+--width 640 \
+--height 360 \
+-t 0 \
+-n \
+--codec libav \
+--libav-format mpegts \
+--libav-video-codec h264_v4l2m2m \
+-o - |
+
+ffmpeg \
+-fflags +genpts+nobuffer+igndts+discardcorrupt \
+-flags low_delay \
+-avioflags direct \
+-hwaccel drm \
+-hwaccel_output_format drm_prime \
+-hide_banner \
+-f alsa \
+-thread_queue_size 8 \
+-i plughw:0 \
+-re \
+-i - \
+-c:v h264_v4l2m2m \
+-b:v 1700k \
+-fpsmax 15 \
+-c:a libopus \
+-b:a 32k \
+-application lowdelay \
+-ar 48000 \
+-f s16le \
+-threads 4 \
+-f rtsp \
+-rtsp_transport tcp \
+rtsp://:8554/mystream
